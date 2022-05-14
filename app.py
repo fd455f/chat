811e17dd18cd9c4c3ff8461509f1bdb0ef3a0445
unknown <you@example.com>
2022-05-14 19:48:01 -0300

    acertos

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def index():
     return "Hello World!"
 
 # Wrap Flask app with Talisman
-Talisman(app, content_security_policy=None)
+#Talisman(app, content_security_policy=None)
 
 if __name__ == '__main__':
     app.run(debug=False)
