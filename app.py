35240210f4e47837b78618da44dca927d12b98f1
unknown <you@example.com>
2022-05-14 21:19:17 -0300

    v9

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from random import choice
 
+
+cont=1
 app = Flask(__name__)
+
+
+
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
-    if (random.choice([True, False])):
+    global cont
+    cont =cont +1
+    if (cont%2==0):
         return "{\"fulfillmentText\": \"Horário não disponível, tente outro.\", \"followupEventInput\" : {\"name\": \"marcar\"}}"
     else:
         return "{\"fulfillmentText\": \"E qual seu nome ?\", \"followupEventInput\" : {\"name\": \"dados\"}}"
